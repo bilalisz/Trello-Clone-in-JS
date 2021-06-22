e334fde82097ff3b2d10b339322a45d9e867c70f
Bilalisz <khan.tech885@gmail.com>
2021-06-22 18:09:39 +0500

    card is create by click

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("I am working...!");
+
+const btnClick = (name) => {
+  console.log("start");
+  let main = document.querySelector(".main-section");
+  let card = document.createElement("div");
+  let titleWrapper = document.createElement("div");
+  let title = document.createElement("h3");
+  title.innerHTML = name;
+  card.setAttribute("class", "card");
+  main.appendChild(card);
+  card.appendChild(titleWrapper);
+  titleWrapper.appendChild(title);
+};
+
+{
+  /* <div class="card">
+  <div>
+    <h3 id="title">Todos</h3>
+  </div>
+</div>; */
+}
