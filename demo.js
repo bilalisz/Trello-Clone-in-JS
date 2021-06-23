9c1e91ad0b469cac8b02c2b9aefd696638ce0dd0
Bilalisz <khan.tech885@gmail.com>
2021-06-23 18:09:27 +0500

    card is create

---
+++ demo.js
@@ -0,0 +1,18 @@
+funDeclear();
+// funLet();
+// funConst();
+funVar();
+
+const funConst = () => {
+  console.log("fun const");
+};
+let funLet = () => {
+  console.log("fun let");
+};
+var funVar = () => {
+  console.log("fun var");
+};
+
+function funDeclear() {
+  console.log("fun declearation");
+}
--- script.js
+++ script.js
@@ -1,22 +1,70 @@
 console.log("I am working...!");
+let cardArray = [];
+let date = new Date();
 
-const btnClick = (name) => {
-  console.log("start");
+const getCardTamplet = (cardData) => {
+  // create Elements
   let main = document.querySelector(".main-section");
   let card = document.createElement("div");
   let titleWrapper = document.createElement("div");
   let title = document.createElement("h3");
-  title.innerHTML = name;
-  card.setAttribute("class", "card");
+  let deleteCardBtn = document.createElement("button");
+
+  //   make Dom hirarchey
   main.appendChild(card);
   card.appendChild(titleWrapper);
   titleWrapper.appendChild(title);
+  titleWrapper.append(deleteCardBtn);
+
+  //   Set Attributies
+  card.setAttribute("class", "card");
+  card.setAttribute("id", cardData.id);
+  title.innerHTML = cardData.title;
+  deleteCardBtn.innerText = "delete";
+  deleteCardBtn.setAttribute("class", "add-task-btn");
+  deleteCardBtn.addEventListener("click", () => handleDelete(cardData.id));
+};
+const handleClickTitle = () => {
+  let input = document.getElementById("id-input");
+  let value = input.value;
+  if (!value) {
+    alert("enter title");
+  } else {
+    let cardObj = {
+      id: Math.random(2, 5),
+      title: value,
+      timeStamp: getTimeStamp(date),
+    };
+    cardArray.push(cardObj);
+    getCardTamplet(cardObj);
+    btnStatus();
+    console.log(cardArray);
+  }
 };
 
-{
-  /* <div class="card">
-  <div>
-    <h3 id="title">Todos</h3>
-  </div>
-</div>; */
-}
+const handleDelete = (id) => {
+  let confirm = window.confirm("Are you sure!");
+  let index = cardArray.indexOf(cardArray.find((card) => card.id === id));
+
+  if (confirm) {
+    document.getElementById(id).remove();
+    cardArray.splice(index, 1);
+    btnStatus();
+  }
+  console.log(id, index);
+  console.log(cardArray);
+};
+
+// utility functions
+
+const getTimeStamp = (dateStr) => {
+  return `${date.getDate()} / ${date.getMonth()} / ${date.getYear()}`;
+};
+
+const btnStatus = () => {
+  let btnCard = document.querySelector("#btn-add-list");
+  if (cardArray.length === 3) {
+    btnCard.disabled = true;
+  } else btnCard.disabled = false;
+  cardArray.length > 3 ? alert("No more task space is allow! sorry..!") : null;
+};
